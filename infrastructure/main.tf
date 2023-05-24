cbeeb764101a4a61cb9ce279456f1f7716549438
Suraj <S_pun@outlook.com>
2023-05-24 23:08:35 +0100

    Code commit module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,3 +12,25 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+// CODECOMMIT
+module "codecommit_repo" {
+  source          = "./modules/code_commit"
+  repository_name = var.repo_name
+  description     = var.repo_description
+  default_branch  = var.branch_name
+}
+
+// NULL Resource to prevent empty repository
+resource "null_resource" "codecommit_interaction" {
+  triggers = {
+    # Trigger the null resource on every run
+    run_on_creation = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+    aws codecommit create-commit --repository-name ${module.codecommit_repo.repository_name} --branch-name ${module.codecommit_repo.default_branch} --put-files "filePath=Readme.md,fileContent=V2VsY29tZSB0byBvdXIgdGVhbSByZXBvc2l0b3J5IQo="
+    EOT
+  }
+}
