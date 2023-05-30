3472d277f88e77e291745e0fd8b4ae7806afdce8
Suraj <S_pun@outlook.com>
2023-05-30 17:21:35 +0100

    Iam user access key output

--- infrastructure/users.tf
+++ infrastructure/users.tf
@@ -16,3 +16,18 @@ resource "aws_iam_user_group_membership" "membership-1" {
   user   = module.dev-1.name
   groups = [aws_iam_group.developers.name]
 }
+
+// User Access Key
+resource "aws_iam_access_key" "dev-1-key" {
+  user = module.dev-1.name
+}
+
+output "dev_1_id" {
+  value     = aws_iam_access_key.dev-1-key.id
+  sensitive = true
+}
+
+output "dev_1_key" {
+  value     = aws_iam_access_key.dev-1-key.secret
+  sensitive = true
+}
